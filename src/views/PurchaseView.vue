0fde48a654c5c3023c9e6a7fe8c9821a9b538e3f
Davi Chagas <davi.almchagas@gmail.com>
2022-08-29 10:43:55 -0300

    Update of the Puchase View

--- src/views/PurchaseView.vue
+++ src/views/PurchaseView.vue
@@ -1,13 +1,64 @@
 <template>
-  <div class="home">
-    <pre>{{$store.state.movies}}</pre>
-  </div>
+  <div class="purchaseContainer">
+    <FormComponent />
+    <CartComponent />
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
+import CartComponent from '../components/CartComponent.vue'
+import FormComponent from '../components/FormComponent.vue'
 
 export default {
-  name: 'PurchaseView'
+  name: 'PurchaseView',
+  components: {
+    FormComponent,
+    CartComponent,
+}
 }
 </script>
+
+<style>
+.purchaseContainer {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+@media (orientation: portrait) {
+  .purchaseContainer {
+    height: 165vh;
+    justify-content: space-evenly;
+    flex-direction: column;
+  }
+
+
+  form, .cartComponent {
+    width: 80vw;
+  }
+
+  .cartComponent {
+    height: 70vh;
+  }
+
+  .itemsCartList {
+    display: flex;
+    flex-direction: column;
+    height: 55vh;
+    padding: 2.5vh 0;
+}
+
+  form {
+    height: 70vh;
+    grid-template-rows: repeat(5, 12vh);
+    grid-template-columns: repeat(2, 38vw);
+    grid-column-gap: 4vw;
+  }
+  
+  form input {
+    padding: 0.75vh 2vh;
+  }
+}
+</style>
